9fc5d0fa167a00172cb1bfb15ea41c9c527123a8
John Gonzalez <johngonzalez@MBP-work.lan>
2024-01-21 21:01:57 -0800

    feat: added custom statusline for neovim

--- flake.nix
+++ flake.nix
@@ -1,25 +1,27 @@
 {
   description = "Dotfiles Config Flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
 
-  outputs = {nixpkgs, ...}: let
-    forAllSystems = nixpkgs.lib.genAttrs [
-      "aarch64-linux"
-      "i686-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
-  in {
-    templates.cfg = {
-        description = ''
-          Dotfiles for NixOS and Nix-Darwin Development Environments
-        '';
-        path = ./cfg;
+  outputs = { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+    in {
+      templates = {
+        cfg = {
+          description = ''
+            Dotfiles for NixOS and Nix-Darwin Development Environments
+          '';
+          path = ./cfg;
+        };
       };
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
-  };
+      formatter =
+        forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+    };
 }
